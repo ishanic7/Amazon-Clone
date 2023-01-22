d14801410189ddd3fe84c026c41eee2822d8f07e
ishanic7 <ishan1154@chitkara.edu.in>
2023-01-22 21:34:02 +0530

    project created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import Home from "./Home";
+import Header from "./Header";
+import Product from "./Product";
+import Login from "./Login";
+import CheckoutProduct from "./CheckoutProduct";
+import Checkout from "./Checkout";
+import Subtotal from "./Subtotal";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Router>
+        <Switch>
+          <Route path="/login">
+          <Login />
+          </Route>
+          <Route path="/">
+            <Header />
+            <Home />
+            <Product />
+            <Checkout />
+            <CheckoutProduct />
+            <Subtotal />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
+// useEffect(() => {
+//   // will only run once when the app component loads...
 
+//   auth.onAuthStateChanged((authUser) => {
+//     console.log("THE USER IS >>> ", authUser);
+
+//     if (authUser) {
+//       // the user just logged in / the user was logged in
+
+//       dispatch({
+//         type: "SET_USER",
+//         user: authUser,
+//       });
+//     } else {
+//       // the user is logged out
+//       dispatch({
+//         type: "SET_USER",
+//         user: null,
+//       });
+//     }
+//   });
+// }, []);
 export default App;
---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// import firebase from "firebase";
+
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//     apiKey: "AIzaSyDimu-Jf93inV5NMo3NYUvKUtPZmBNR3d4",
+//     authDomain: "clone-92cfc.firebaseapp.com",
+//     projectId: "clone-92cfc",
+//     storageBucket: "clone-92cfc.appspot.com",
+//     messagingSenderId: "436393580288",
+//     appId: "1:436393580288:web:58c21157f97f16426b45e5",
+//     measurementId: "G-4L21RFM15X"
+//   };
+  
+// const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+// const db = firebaseApp.firestore();
+// const auth = firebase.auth();
+
+// export { db, auth };
